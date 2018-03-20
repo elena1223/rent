6f17827b19a36e99bc5ad53c8e12caed848f0c84
d <d@HOME>
2018-03-20 17:08:11 +0900

    로그인 로그아웃 완료

---
+++ src/total/service/LoginOutService.java
@@ -0,0 +1,19 @@
+package total.service;
+
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class LoginOutService {
+
+	@Autowired
+	SqlSessionTemplate template;
+	
+	public Map findByIdAndPass(Map data) {
+		Map map = template.selectOne("member.loginCheck", data);
+		return map;
+	}
+
+}
