a5a04ebe4cc29e28f48f7e80618a26ba1f16eec0
elena <dlstka210@naver.com>
2018-03-28 21:14:14 +0900

    방명록 만드다 맘 ㅡㅡ 에러터져있음

--- src/total/controller/VisitController.java
+++ src/total/controller/VisitController.java
@@ -4,11 +4,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mongodb.MongoClient;
+
+import total.service.MongoCollection;
+import total.service.MongoDatabase;
+
 @Controller
 public class VisitController {
 
 	@RequestMapping("/visit")
 	public String wayController(Model model){
+		
+		
+		
+		
+		 try {
+			MongoClient client = new MongoClient("13.125.168.55", 27017);
+			MongoDatabase db = client.getDatabase("work");
+			MongoCollection collection = db.getCollection("natural");
+
+			
+			
+			 client.close();	
+			 
+		} catch (Exception e) {
+			e.printStackTrace();
+			
+		}
+		 
+		 
+		 
 		model.addAttribute("main","visit.jsp");
 		return "default";
 	}
--- src/total/service/VisitService.java
+++ src/total/service/VisitService.java
@@ -4,13 +4,16 @@ import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class VisitService {
 	
-//	public Map readOne(String id) {
-//		return map;
-//		
-//	}
+	public Map writeVisit(Map data) {
+
+		 
+		return null;
+		
+	}
 	
 
 }
